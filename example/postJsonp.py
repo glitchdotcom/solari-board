934d82872d83bb9a1ff72c1ae818467d15d86ad7
Will <wct616@gmail.com>
2013-08-19 16:20:54 -0400

    formatting fixed

--- example/postJsonp.py
+++ example/postJsonp.py
@@ -10,10 +10,11 @@ import sys
 def cgi():
     data = [   {'sDate':'today','sTime':'13:30','sDeparture':'foo@example.com','nStatus':1,'nTrack':17, 'fLight':True},
                    {'sDate':'yesterday','sTime':'16:00','sDeparture':'bar@example.com','nStatus':2,'nTrack':19, 'fLight':False},
-                   {'sDate':'July 8th, 2013','sTime':'16:30','sDeparture':'baz@example.com','nStatus':2,'nTrack':23, 'fLight':False}]
-                   
+                   {'sDate':'July 8th, 2013','sTime':'16:30','sDeparture':'baz@example.com','nStatus':2,'nTrack':23, 'fLight':False}
+            ]
+
     print("Content-Type: application/json", end='\n\n')
-    print("jsonpCallback(" +json.dumps(data, sys.stdout) + ")")
+    print("jsonpCallback(" + json.dumps(data, sys.stdout) + ")")
 
 if __name__ == '__main__':
     cgi()
